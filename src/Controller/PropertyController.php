0d102babec341e429ee3b3473bf59857dcfe5f51
Marie <Marue59@hotmail.fr>
2022-11-16 22:26:36 +0100

    creation du formulaire pour l'admin et du crud

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -25,31 +25,9 @@ class PropertyController extends AbstractController
     }
 
     #[Route('/biens', name: 'property.index')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(): Response
     {
-        /*
-        // Test insertion bdd
-        $entityManager = $doctrine->getManager();
-        $property = new Property;
 
-        $property->setTitle('Mon premier bien')
-            ->setCity('Calais')
-            ->setAdress('10 rue verte')
-            ->setPostalCode('62100')
-            ->setRooms(5)
-            ->setBedrooms(3)
-            ->setDescription('une petite description')
-            ->setSurface(100)
-            ->setHeat(1)
-            ->setPrice(200000)
-            ->setSold(1);
-
-
-        $entityManager->persist($property);
-        $entityManager->flush();
-       
-        $property = $this->repository->findAllVisible();
-        */
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties',
         ]);
---
+++ src/Form/PropertyType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Property;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PropertyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', null, [
+                'label' => 'Titre du bien'
+            ])
+            ->add('description', null, [
+                'label' => 'Description'
+            ])
+            ->add('surface', null, [
+                'label' => 'Surface'
+            ])
+            ->add('rooms', null, [
+                'label' => 'Nombre de pièce(s)'
+            ])
+            ->add('bedrooms', null, [
+                'label' => 'Nombre de chambre(s)'
+            ])
+            ->add('price', null, [
+                'label' => 'Prix'
+            ])
+            ->add('heat', ChoiceType::class, [
+                'choices' => $this->getChoices(),
+                'label' => 'Type de chauffage',
+
+            ])
+            ->add('city', null, [
+                'label' => 'Ville'
+            ])
+            ->add('adress', null, [
+                'label' => 'Adresse'
+            ])
+            ->add('postal_code', null, [
+                'label' => 'Code postal'
+            ])
+            ->add('sold', null, [
+                'label' => 'Vendu'
+            ])
+            ->add('created_at', null, [
+                'label' => 'Créer le : '
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class
+        ]);
+    }
+
+    // utilisation de la clé comme valeur et de la valeur comme clé ..
+    private function getChoices()
+    {
+        $choices = Property::HEAT;
+        $output = [];
+        foreach ($choices as $key => $value) {
+            $output[$value] = $key;
+        }
+        return $output;
+    }
+}
