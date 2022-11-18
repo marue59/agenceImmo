ce0e6771330738ed20b6a30bf0cb11fe27e4eac4
Marie <Marue59@hotmail.fr>
2022-11-18 23:24:27 +0100

    feat:ajout de l'authentification

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    /**
+     * @var UserPasswordHasherInterface
+     */
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('admin@test.fr');
+        $password = $this->hasher->hashPassword($user, '1234');
+        $user->setPassword($password);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
